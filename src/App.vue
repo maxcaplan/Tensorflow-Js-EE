69822945f2fafffc2d49b723f8465feeaeb37891
jellycream <maxacaplan@gmail.com>
2018-08-17 07:21:14 -0300

    Initial Comit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <app-navbar></app-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/NavBar.vue";
+// import Footer from "./components/Footer.vue";
+// import Home from "./components/Main/Home.vue";
+
+export default {
+  components: {
+    "app-navbar": Navbar
+    // "app-footer": Footer,
+    // "app-home": Home
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
